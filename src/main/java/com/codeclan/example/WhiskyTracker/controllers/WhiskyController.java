93ca5db0555f76c3537c0608793fb7377ec73346
crispymccoy145 <samspam19941@gmail.com>
2021-06-23 15:39:20 +0100

    mvp completed

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,32 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskys")
+    public ResponseEntity<List<Whisky>> getAllWhiskys(){
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskys/{id}")
+    public ResponseEntity getWhiskey(@PathVariable Long id){
+        return new ResponseEntity<>(whiskyRepository.findById(id), HttpStatus.OK);
+    }
+
+
+}
---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,50 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskyByYear(){
+		List<Whisky> found = whiskyRepository.findByYear(2018);
+		assertEquals(6, found.size());
+	}
+
+	@Test
+	public void canFindDistilleryByRegion() {
+		List<Distillery> found = distilleryRepository.findByRegion("Island");
+		assertEquals(3, found.size());
+	}
+
+	@Test
+	public void canFindWhiskyByAgeAndDistillery() {
+		Optional<Distillery> foundDistillery = distilleryRepository.findById(2L);
+		List<Whisky> foundWhisky = whiskyRepository.findByAgeAndDistillery(12, foundDistillery);
+		assertEquals(1, foundWhisky.size());
+	}
+}
